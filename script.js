bd8cdedf7b514f59d34c3e0d91b9377e0de4fa13
krzysztofmotas <127015975+krzysztofmotas@users.noreply.github.com>
2024-01-29 19:10:44 +0100

    popraweczki

--- script.js
+++ script.js
@@ -1,12 +1,5 @@
 const scrollToTop = document.querySelector(".scroll-to-top");
 
 window.addEventListener("scroll", () => {
-    if (window.scrollY > window.outerHeight / 3) {
-
-        if (!scrollToTop.classList.contains("active")) {
-            scrollToTop.classList.add("active");
-        }
-    } else if (scrollToTop.classList.contains("active")) {
-        scrollToTop.classList.remove("active");
-    }
+    scrollToTop.classList.toggle("active", window.scrollY > window.outerHeight / 3);
 });
